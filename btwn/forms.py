1b4c859a3270bfd11ca2930fc29d8e29e45d2446
janek_g <jmgrzesik@gmail.com>
2016-01-05 22:20:00 +0100

    Adds basic functionalities like adding comments, adding posts, showing feeds for users

--- btwn/forms.py
+++ btwn/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-from .models import UserProfile
+from .models import UserProfile, Post, Comment
 
 
 class UserForm(forms.ModelForm):
@@ -11,7 +11,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        exclude = []
+        fields = ['username', 'password', 'first_name', 'last_name', 'email']
 
     def clean_password_confirmation(self):
         confirmation = self.cleaned_data['password_confirmation']
@@ -30,10 +30,28 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ['user']
+
+        fields = ['profile_name']
 
     def clean_profile_name(self):
         data = self.cleaned_data['profile_name']
         if UserProfile.objects.filter(profile_name=data).exists():
             raise ValidationError("Profile name already taken")
         return data
+
+
+class LoginForm(forms.Form):
+    login = forms.CharField(label='User name', max_length=100, required=True)
+    password = forms.CharField(label='Your password', max_length=100, required=True)
+
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment_text']
+
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'post_text', 'public']
--- btwn/urls.py
+++ btwn/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'btwn'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Newest.as_view(), name='home'),
     url(r'^(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post_detail'),
-    url(r'^(?P<post_id>[0-9]+)/comment$', views.CommentCreate.as_view(), name='comment_create'),
+    url(r'^(?P<post_id>[0-9]+)/comment$', views.comment_create_view, name='comment_create'),
     url(r'^comment/(?P<pk>[0-9]+)$', views.CommentDetailView.as_view(), name='comment_detail'),
     url(r'^newest$', views.Newest.as_view(), name='post_newest'),
+    url(r'^post_create', views.post_create_view, name='post_create'),
     url(r'^logout$', views.logout_view, name='logout'),
     url(r'^register$', views.register_view, name='register'),
-    url(r'^login$', auth_views.login, {
-        'template_name': 'btwn/login.html'
-    }, name='login'),
-
+    url(r'^login$', views.login_view, name='login'),
+    url(r'^profile', views.login_view, name='profile'),  # todo
+    url(r'^feed/(?P<pk>[0-9]+)$', views.feed, name='feed')
 ]
