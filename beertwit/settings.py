89714ec05a2b64b40745f371adeddfbd12a182fe
janek_g <jmgrzesik@gmail.com>
2016-01-07 00:39:13 +0100

    Adds materialize to the project

--- beertwit/settings.py
+++ beertwit/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&17#f^wfpx_787$pzh$)ypvudddf#3wfh81qgy5+v_by5*76bw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- btwn/forms.py
+++ btwn/forms.py
@@ -11,7 +11,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
     def clean_password_confirmation(self):
         confirmation = self.cleaned_data['password_confirmation']
